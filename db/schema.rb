b8e0dcc32c8b613e1a9df6bf5d3ef42334243a78
Kateryna Strough <katerynastrough@hotmail.com>
2019-09-23 22:57:33 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -17,6 +18,7 @@ ActiveRecord::Schema.define(version: 1) do
     t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "active",     default: false
   end
 
 end
