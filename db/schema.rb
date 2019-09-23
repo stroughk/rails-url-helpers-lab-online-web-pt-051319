f230f9fe70ebe3f4d369c05a43baae2e8453a6dc
Kateryna Strough <katerynastrough@hotmail.com>
2019-09-23 22:58:05 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 1) do
     t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "active",     default: false
   end
 
 end
